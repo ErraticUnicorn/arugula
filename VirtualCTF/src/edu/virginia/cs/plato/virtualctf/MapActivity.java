93c33497913ef36e2020b97698987e9974830494
jackmacattack <jackmacattack92@gmail.com>
2014-04-02 23:27:05 -0400

    Finished phase 4

--- VirtualCTF/src/edu/virginia/cs/plato/virtualctf/MapActivity.java
+++ VirtualCTF/src/edu/virginia/cs/plato/virtualctf/MapActivity.java
@@ -2,6 +2,7 @@ package edu.virginia.cs.plato.virtualctf;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -140,6 +141,11 @@ public class MapActivity extends FragmentActivity implements
 			((MapActivity) activity).onSectionAttached(getArguments().getInt(
 					ARG_SECTION_NUMBER));
 		}
+	}
+	
+	public void goToMain(View v) {
+		Intent i = new Intent(getApplicationContext(), MainActivity.class); 
+		startActivity(i);
 	}
 
 }
