6c4803ccf41601633bc37e0a6d22f592016cb90d
jackmacattack <jackmacattack92@gmail.com>
2014-04-04 01:33:31 -0400

    Added Google Maps

--- VirtualCTF/src/edu/virginia/cs/plato/virtualctf/NewGameBoundsActivity.java
+++ VirtualCTF/src/edu/virginia/cs/plato/virtualctf/NewGameBoundsActivity.java
@@ -1,8 +1,11 @@
 package edu.virginia.cs.plato.virtualctf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,6 +15,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.JsonArray;
 
 import edu.virginia.cs.plato.virtualctf.util.HTTPManager;
@@ -19,10 +29,15 @@ import edu.virginia.cs.plato.virtualctf.util.JsonCallback;
 
 public class NewGameBoundsActivity extends FragmentActivity {
 
+	private static GoogleMap map;
+
 	private String name;
 	private String pw;
 
-	private boolean teamOne;
+	private List<LatLng> teamOne;
+	private List<LatLng> teamTwo;
+
+	private int mode = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +45,28 @@ public class NewGameBoundsActivity extends FragmentActivity {
 		setContentView(R.layout.activity_new_game_bounds);
 
 		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
+			getFragmentManager().beginTransaction()
 			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
 			name = extras.getString("Name");
-//			pw = extras.getString("PW");
+			//			pw = extras.getString("PW");
 		}
+
+		teamOne = new ArrayList<LatLng>();
+		teamTwo = new ArrayList<LatLng>();
+		
+	}
+
+	protected void onStart() {
+		super.onStart();
+
+		MapsInitializer.initialize(this);
+
+		getFragmentManager().executePendingTransactions();
+		onMapLoad();
 	}
 
 	@Override
@@ -74,12 +102,48 @@ public class NewGameBoundsActivity extends FragmentActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_new_game_bounds,
 					container, false);
+
 			return rootView;
 		}
 	}
 
+
+	public void onMapLoad() {
+
+		// Get a handle to the Map Fragment
+		map = ((MapFragment) getFragmentManager()
+				.findFragmentById(R.id.map))
+				.getMap(); 
+
+		if(map != null) {
+			map.setOnMapClickListener(new OnMapClickListener() {
+
+				@Override
+				public void onMapClick(LatLng p) {
+					if(mode == 1) {
+						teamTwo.add(p);
+						map.addMarker(new MarkerOptions()
+						.position(p)
+						.title("Team 2") 
+						.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+					} 
+					else if(mode == 0) {
+						teamOne.add(p);
+						map.addMarker(new MarkerOptions()
+						.position(p)
+						.title("Team 1")
+						.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+					}
+				}
+
+			});
+		}
+
+	}
 	public void onSubmit(View v) {
-		if(teamOne) {
+
+		if(mode == 1) {
 			HTTPManager.createNewGame(new JsonCallback() {
 
 				@Override
@@ -89,14 +153,14 @@ public class NewGameBoundsActivity extends FragmentActivity {
 
 			}, name, pw);
 		}
-		else {
+		else if(mode == 0) {
 			TextView title = (TextView) findViewById(R.id.bounds_title);
 			Button sub = (Button) findViewById(R.id.submit_bounds);
 
 			title.setText(R.string.boundsTitle2);
 			sub.setText(R.string.create_game);
 
-			teamOne = true;
+			mode = 1;
 		}
 
 	}
